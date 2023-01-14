a494d690385d27f429c1c6fa4e8c870f54fa9fac
ddobrikov <ddobrikov@dxc.com>
2023-01-14 14:06:51 +0200

    change in requiremnts.txt and url.py

--- openligadb/urls.py
+++ openligadb/urls.py
@@ -4,8 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('ratio', views.win_loss_ratio, name='ratio'), 
+    path('ratio', views.win_loss_ratio, name='ratio'),
     path('upcoming', views.next_game_day, name='upcoming'),
-    path('search_team', views.search_team, name='search_team'), 
-    
+    path('search_team', views.search_team, name='search_team'),
 ]
